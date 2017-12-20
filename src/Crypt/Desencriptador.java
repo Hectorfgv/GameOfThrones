0574d435f2cb7177e1775913054aae574e88eced
DESKTOP-P7R113A\Alex <alejandromartinezpalanques@gmail.com>
2017-12-20 15:54:52 +0100

    Subimos Encriptador y Desencriptador finales.

--- src/Crypt/Desencriptador.java
+++ src/Crypt/Desencriptador.java
@@ -3,11 +3,11 @@ package Crypt;
 public class Desencriptador {
 
 	//AQUI SE DEFINIRÁ LAS VARIABLES A UTILIZAR
-	
+
 	//AQUÍ LAS INICIALIZACIONES
 	public Desencriptador() {
 	}
-	
+
 	/**
 	 * En esta función implementaremos nuestro proceso de desencriptación a partir
 	 * de la frase de entrada
@@ -16,7 +16,103 @@ public class Desencriptador {
 	 * @return       el String desencriptado
 	 */
 	public String desencriptarFrase(String frase) {
-		return frase;
+		String fraseDeCod,invertida,creaFrase;
+		//Comienza la codificacion
+
+		creaFrase=crear(frase);		
+		invertida=invert(creaFrase);
+		fraseDeCod=cesar(invertida);
+		return fraseDeCod;
+	}
+	public String crear(String frase) {
+
+		String f,fraseDeCod="";
+		f=frase;
+		int cont=0;
+
+		for(int i=0;i<frase.length();i++) {
+
+			char letra=f.charAt(i);
+			if (cont==1) {
+
+				fraseDeCod=fraseDeCod+letra;
+				cont++;
+			}
+			else if (cont!=2) {
+
+				cont++;
+			}
+			else {
+				cont=0;
+			}
+
+		}
+		return fraseDeCod;
+	}
+	public String invert(String creaFrase) {
+
+		String f;
+		f=creaFrase;
+
+		String fraseInvertida="";
+
+		for (int x=f.length()-1;x>=0;x--) {
+
+			fraseInvertida = fraseInvertida + f.charAt(x);
+
+		}
+		return fraseInvertida;
+	}
+	public String cesar(String invertida ) {
+
+		String f;
+		f=invertida;
+		String alfabeto = "" ;
+		String alfabetoCod = "" ;
+		String alfabetoTemp = "" ;
+
+		String fraseDeCodificar = "" ;
+		String fraseDeCodificada = "" ;
+		int paso = 4 ;
+		int posicionLetra = 0 ;
+
+		for (char i=31; i<=126;i++) {
+			alfabeto=alfabeto+i;
+		}
+		// Generar el alfabeto de codificacion
+
+		for(int i=paso;i<alfabeto.length();i++) {
+
+			alfabetoCod=alfabetoCod+alfabeto.charAt(i);
+		}
+		alfabetoTemp=alfabeto;
+		alfabeto=alfabetoCod;
+		alfabetoCod=alfabetoTemp;
+
+		for(int i=0;i<paso;i++) {
+
+			alfabetoCod=alfabetoCod+alfabeto.charAt(i);
+		}
+
+
+		// Comienza la codificacion
+
+		fraseDeCodificar = f;
+
+		for(int i=0;i<fraseDeCodificar.length();i++) {
+
+			posicionLetra=alfabeto.indexOf(fraseDeCodificar.charAt(i));
+
+			fraseDeCodificada=fraseDeCodificada+alfabetoCod.charAt(posicionLetra);
+
+		}
+
+
+		return fraseDeCodificada;
 	}
 
+
+
 }
+
+
--- src/Crypt/Encriptador.java
+++ src/Crypt/Encriptador.java
@@ -3,9 +3,9 @@ package Crypt;
 public class Encriptador {
 
 	public Encriptador() {
-		
+
 	}
-	
+
 	/**
 	 * En esta función implementaremos nuestro proceso de encriptcaión a partir
 	 * de la frase de entrada
@@ -14,7 +14,80 @@ public class Encriptador {
 	 * @return       el String encriptado
 	 */
 	public String encriptarFrase(String frase) {
-		return frase;
+		String fraseCod,invertida,creaFrase;
+		creaFrase=crear(frase);
+		invertida=invert(creaFrase);
+		fraseCod=cesar(invertida);
+		return fraseCod;
+	}
+	public String crear(String frase) {
+
+		String f,frasecod="";
+		f=frase;
+
+		for(int i=0;i<frase.length();i++) {
+
+			char letra=f.charAt(i);
+			frasecod=frasecod+((char)(letra-1))+letra+((char)(letra+1));
+
+		}
+		return frasecod;
+	}
+	public String invert(String creaFrase) {
+
+		String f;
+		f=creaFrase;
+
+		String fraseInvertida="";
+
+		for (int x=f.length()-1;x>=0;x--) {
+
+			fraseInvertida = fraseInvertida + f.charAt(x);
+
+		}
+		return fraseInvertida;
+	}
+	public String cesar(String invertida ) {
+
+		String f;
+		f=invertida;
+		String alfabeto = "" ;
+		String alfabetoCod = "" ;
+
+		String fraseCodificar = "" ;
+		String fraseCodificada = "" ;
+		int paso = 4 ;
+		int posicionLetra = 0 ;
+
+		for (char i=31; i<=126;i++) {
+			alfabeto=alfabeto+i;
+		}
+		// Generar el alfabeto de codificacion
+
+		for(int i=paso;i<alfabeto.length();i++) {
+
+			alfabetoCod=alfabetoCod+alfabeto.charAt(i);
+		}
+		for(int i=0;i<paso;i++) {
+
+			alfabetoCod=alfabetoCod+alfabeto.charAt(i);
+		}
+
+
+		// Comienza la codificacion
+
+		fraseCodificar = f;
+
+		for(int i=0;i<fraseCodificar.length();i++) {
+
+			posicionLetra=alfabeto.indexOf(fraseCodificar.charAt(i));
+
+			fraseCodificada=fraseCodificada+alfabetoCod.charAt(posicionLetra);
+
+		}
+
+
+		return fraseCodificada;
 	}
 
 }
